ed0bec873a0ecbc58bc9078600811341ad69e18b
Gulam Hussain <hussainkhan1200@gmail.com>
2018-03-07 11:31:13 +0530

    Database class

--- classes/Database.php
+++ classes/Database.php
@@ -1,12 +1,51 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: hussain-dev
- * Date: 07-Mar-18
- * Time: 8:38 AM
+ *
  */
-
 class Database
 {
+//  DATABASE CONNECTION VARIABLE
 
+
+    function __construct()
+    {
+        global $db;
+        try
+        {
+            $db = new PDO("mysql:host=localhost;dbname=student-info-system;", 'root', '1mlRgbdmMBj2LQeW');
+            $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+        }
+        catch(PDOException $e)
+        {
+            echo "Database Connection failed";
+            exit();
+        }
+    }
+
+    function runPrepare($data,$sql)
+    {
+        global $db;
+
+        $stmt=$db->prepare($sql);
+        $result=$stmt->execute($data);
+        if($result)
+        {
+            return $stmt;
+        }
+        else
+        {
+            return False;
+        }
+    }
+
+    function runQuery($sql)
+    {
+        global $db;
+        $result=$db->query($sql);
+        return $result;
+    }
 }
+
+$dbob=new Database();
+?>
